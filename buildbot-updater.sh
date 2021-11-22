7cee2698e5a480ac0e609af273facf9f6cafaf14
tannisroot <10602045+tannisroot@users.noreply.github.com>
2021-11-22 19:17:55 +0300

    Handle force push situations when updating

--- buildbot-updater.sh
+++ buildbot-updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 vagrant up
-vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"cd buildbot && git pull origin master\""
-vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"cd buildbot && git pull origin master\""
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
 vagrant halt
 echo "Buildbot updated"
