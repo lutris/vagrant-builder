b05a45cbb55cb2ad9e14e1ace11fa60c91610879
tannisroot <10602045+tannisroot@users.noreply.github.com>
2021-12-20 12:01:17 +0300

    Run apt update before building

--- buildbot-updater.sh
+++ buildbot-updater.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 vagrant up
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"sudo apt update\""
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"sudo apt update\""
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"cd buildbot && git fetch origin && git reset --hard origin/master\""
 vagrant halt
--- make_libretro.sh
+++ make_libretro.sh
@@ -2,6 +2,8 @@
 vagrant up
 
 # start build
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"sudo apt update\""
+vagrant ssh -c "ssh ubuntu@buildbot-bionic-i386 \"sudo apt update\""
 vagrant ssh -c "ssh ubuntu@buildbot-bionic-amd64 \"cd buildbot/runners/libretro && ./build.sh -d -n\""
 vagrant ssh -c 'ssh ubuntu@buildbot-bionic-amd64 "mv buildbot/runners/libretro/*.tar.xz ~/"'
 vagrant ssh -c 'scp ubuntu@buildbot-bionic-amd64:/home/ubuntu/*.tar.xz /vagrant/'
